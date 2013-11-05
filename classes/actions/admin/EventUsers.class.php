5bd91ec7aa9861d123b227e5480282c1929c1bb1
PSNet <light.feel@gmail.com>
2013-11-06 00:54:38 +0200

    - фикс.
    Signed-off-by: PSNet <light.feel@gmail.com>

--- classes/actions/admin/EventUsers.class.php
+++ classes/actions/admin/EventUsers.class.php
@@ -1124,7 +1124,7 @@ class PluginAdmin_ActionAdmin_EventUsers extends Event {
 				/*
 				 * получить статистику стран или городов
 				 */
-				$oViewer->Assign('aLivingStats', $aLivingStatsData['aLivingStats']);
+				$oViewer->Assign('aData', $aLivingStatsData['aLivingStats']);
 				/*
 				 * тип текущего отображения: страны или города
 				 */
@@ -1141,7 +1141,7 @@ class PluginAdmin_ActionAdmin_EventUsers extends Event {
 				 * для расчетов нужно количество всех пользователей, берем их уже из кеша
 				 */
 				$aStats = $this->User_GetStatUsers();
-				$oViewer->Assign('iTotalUsersCount', $aStats['count_all']);
+				$oViewer->Assign('iTotal', $aStats['count_all']);
 				/*
 				 * вернуть скомпилированный шаблон
 				 */
