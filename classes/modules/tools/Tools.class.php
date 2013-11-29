f5be4d1ba13cc372ee815a8d4d76d5ca536f5d4d
PSNet <light.feel@gmail.com>
2013-11-29 19:29:13 +0200

    - начал делать проверку файлов на некорректную кодировку
    Signed-off-by: PSNet <light.feel@gmail.com>

--- classes/modules/tools/Tools.class.php
+++ classes/modules/tools/Tools.class.php
@@ -76,6 +76,128 @@ class PluginAdmin_ModuleTools extends Module {
 		return false;
 	}
 
+
+	/**
+	 * Выполнить проверку конфигов и языковых файлов плагинов и системы на utf-8 w/o BOM
+	 */
+	public function CheckLangsAndConfigsOfPluginsAndEngineToBeInCorrectEncoding() {
+		/*
+		 * проверить файлы движка
+		 */
+		$aFilesMasksToCheck = array(
+			/*
+			 *
+			 * --- проверка файлов фреймворка ---
+			 *
+			 */
+			/*
+			 * все конфиги: конфиг, жевикс, загрузчик
+			 */
+			Config::Get('path.framework.server') . 'config/*.php',
+			/*
+			 * все css файлы
+			 */
+			Config::Get('path.framework.server') . 'frontend/framework/*.css',
+			/*
+			 * все js файлы (папку vendor не будем проверять)
+			 */
+			Config::Get('path.framework.server') . 'frontend/framework/js/core/*.js',
+			Config::Get('path.framework.server') . 'frontend/framework/js/ui/*.js',
+			/*
+			 * языковые файлы
+			 */
+			Config::Get('path.framework.server') . 'frontend/i18n/*.php',
+			/*
+			 * шаблоны
+			 */
+			Config::Get('path.framework.server') . 'frontend/templates/*.tpl',
+			Config::Get('path.framework.server') . 'frontend/templates/*.js',
+			Config::Get('path.framework.server') . 'frontend/templates/*.css',
+
+			/*
+			 *
+			 * --- проверка файлов приложения ---
+			 *
+			 */
+			/*
+			 * все конфиги: конфиг, жевикс
+			 */
+			Config::Get('path.application.server') . 'config/*.php',
+			/*
+			 * все js файлы
+			 */
+			Config::Get('path.application.server') . 'frontend/common/js/*.js',
+			/*
+			 * языковые файлы
+			 */
+			Config::Get('path.application.server') . 'frontend/i18n/*.php',
+			/*
+			 * шаблоны
+			 */
+			Config::Get('path.application.server') . 'frontend/skin/*.tpl',
+			Config::Get('path.application.server') . 'frontend/skin/*.js',
+			Config::Get('path.application.server') . 'frontend/skin/*.css',
+			/*
+			 * проверить файлы крона
+			 */
+			Config::Get('path.application.server') . 'utilities/cron/*.php',
+		);
+		$this->CheckFilesEncodingByArray($aFilesMasksToCheck);
+
+		/*
+		 * todo: конфиги и языковые файлы плагинов
+		 */
+	}
+
+
+	/**
+	 * Проверить файлы из переданного массива на корректность кодировки
+	 *
+	 * @param $aFilesMasksToCheck	массив файлов для проверки
+	 */
+	protected function CheckFilesEncodingByArray($aFilesMasksToCheck) {
+		foreach ($aFilesMasksToCheck as $sFileMask) {
+			$aFilesMasksToCheck = glob($sFileMask);
+			foreach ($aFilesMasksToCheck as $sFile) {
+				/*
+				 * можно ли прочитать этот файл
+				 */
+				if (!is_readable($sFile)) {
+					$this->Message_AddError($this->Lang_Get('plugin.admin.errors.encoding_check.unreadable_file', array('file' => $sFile)), $this->Lang_Get('error'));
+					continue;
+				}
+				/*
+				 * получить первые 50 байт, этого достаточно для проверки
+				 */
+				if (($sText = @file_get_contents($sFile, false, null, 0, 50)) === false) {
+					$this->Message_AddError($this->Lang_Get('plugin.admin.errors.encoding_check.file_cant_be_read', array('file' => $sFile)), $this->Lang_Get('error'));
+					continue;
+				}
+				/*
+				 * проверить на некорректную кодировку файлов
+				 */
+				if ($this->CheckTextEncodingForUTF8BOM($sText)) {
+					$this->Message_AddError($this->Lang_Get('plugin.admin.errors.encoding_check.utf8_bom_encoding_detected', array('file' => $sFile)), $this->Lang_Get('error'));
+					continue;
+				}
+			}
+		}
+	}
+
+
+	/**
+	 * Проверить является ли кодировкой текста utf-8 BOM, которую нельзя использовать в файлах движка
+	 *
+	 * @param $sText	текст для проверка
+	 * @return bool
+	 */
+	protected function CheckTextEncodingForUTF8BOM($sText) {
+		/*
+		 * utf-8 BOM отличается от простой utf-8 без сигнатуры первыми тремя символами
+		 */
+		return substr($sText, 0, 3) === pack('CCC', 0xEF, 0xBB, 0xBF);
+	}
+
 }
 
 ?>
