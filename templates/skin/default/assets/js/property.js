6192c8ec9f1f9ded4081f11a296c2224942f420f
PSNet <light.feel@gmail.com>
2014-05-05 14:13:27 +0300

    - фикс сортировки типов топиков и дополнительных полей новых типов топиков: не нужно обновлять данные если сортировка элементов не поменялась - удалены вызовы логгирования в отладчике: если у пользователя не было бы отладчика, то жс падал бы с ошибкой при попытке изменения сортировки - фикс форматирования
    Signed-off-by: PSNet <light.feel@gmail.com>

--- templates/skin/default/assets/js/property.js
+++ templates/skin/default/assets/js/property.js
@@ -1,21 +1,21 @@
 /**
  * Дополнительные поля
- * 
+ *
  * @module ls/plugin/admin/property
- * 
+ *
  * @license   GNU General Public License, version 2
  * @copyright 2013 OOO "ЛС-СОФТ" {@link http://livestreetcms.com}
  */
 
 var ls = ls || {};
 
-ls.admin_property = ( function ($) {
+ls.admin_property = ( function($) {
 
 	this.clickSelectItemNew = function(el) {
 		if (el) {
-			var el=$(el);
+			var el = $(el);
 		} else {
-			var el=$('#property-select-items');
+			var el = $('#property-select-items');
 		}
 		var $aItems = el.find('.js-property-select-item');
 
@@ -30,21 +30,29 @@ ls.admin_property = ( function ($) {
 		return false;
 	};
 
+
 	this.clickSelectItemRemove = function(obj) {
 		obj = $(obj);
 
-		var area=obj.parents('.js-property-select-area');
+		var area = obj.parents('.js-property-select-area');
 
 		if (area.find('.js-property-select-item').length < 2) return false;
 
-		obj.parent().fadeOut(function(){
+		obj.parent().fadeOut(function() {
 			$(this).remove();
 		});
 
 		return false;
 	};
 
+
 	this.initTableProperty = function() {
+		/**
+		 * todo:
+		 * 		1. вынести хелпер в общие, чтобы можно было повторно использовать в сортировке типов топиков и здесь
+		 * 		т.к. данный хелпер может быть полезен и для плагинов
+		 *
+		 */
 		var fixHelper = function(e, ui) {
 			ui.children().each(function() {
 				$(this).width($(this).width());
@@ -53,20 +61,27 @@ ls.admin_property = ( function ($) {
 		};
 
 		$('#property-list tbody').sortable({
+			cursor: 'move',
 			helper: fixHelper,
-			stop: this.stopTableTypeSort
+			update: this.UpdateTableTypeSort
 		}).disableSelection();
 	};
 
-	this.stopTableTypeSort = function(e,ui) {
-		var items=$('#property-list tbody tr');
-		var data=[];
-		items.each(function(k,v){
-			data.push({ id: $(v).data('id'), sort: items.length-k });
+
+	this.UpdateTableTypeSort = function(e, ui) {
+		var items = $('#property-list tbody tr');
+		var data = [];
+		/**
+		 * todo:
+		 * 		2. вынести метод получения ид для отправки на сортировку в метод для повторного использования при сортировке типов топиков
+		 * 		т.к. данный метод может быть полезен и для плагинов
+		 *
+		 */
+		items.each(function(k, v) {
+			data.push({ id: $(v).data('id'), sort: items.length - k });
 		});
-		console.log(data);
 		if (data.length) {
-			ls.ajax.load(aRouter.admin+'ajax/properties/sort-save/', { data: data }, function(result) {
+			ls.ajax.load(aRouter.admin + 'ajax/properties/sort-save/', { data: data }, function(result) {
 				if (result.bStateError) {
 					ls.msg.error(null, result.sMsg);
 				} else {
@@ -77,4 +92,5 @@ ls.admin_property = ( function ($) {
 	};
 
 	return this;
-}).call(ls.admin_property || {},jQuery);
+
+}).call(ls.admin_property || {}, jQuery);
--- templates/skin/default/assets/js/topic.js
+++ templates/skin/default/assets/js/topic.js
@@ -1,17 +1,24 @@
 /**
  * Дополнительные поля
- * 
+ *
  * @module ls/plugin/admin/property
- * 
+ *
  * @license   GNU General Public License, version 2
  * @copyright 2013 OOO "ЛС-СОФТ" {@link http://livestreetcms.com}
  */
 
 var ls = ls || {};
 
-ls.admin_topic = ( function ($) {
+ls.admin_topic = ( function($) {
 
 	this.initTableType = function() {
+		/**
+		 * todo: см. property.js
+		 * 
+		 * @param e
+		 * @param ui
+		 * @returns {*}
+		 */
 		var fixHelper = function(e, ui) {
 			ui.children().each(function() {
 				$(this).width($(this).width());
@@ -20,20 +27,24 @@ ls.admin_topic = ( function ($) {
 		};
 
 		$('#type-list tbody').sortable({
+			cursor: 'move',
 			helper: fixHelper,
-			stop: this.stopTableTypeSort
+			update: this.UpdateTableTypeSort
 		}).disableSelection();
 	};
+	
 
-	this.stopTableTypeSort = function(e,ui) {
-		var items=$('#type-list tbody tr');
-		var data=[];
-		items.each(function(k,v){
-			data.push({ id: $(v).data('typeId'), sort: items.length-k });
+	this.UpdateTableTypeSort = function(e, ui) {
+		var items = $('#type-list tbody tr');
+		var data = [];
+		/**
+		 * * todo: см. property.js
+		 */
+		items.each(function(k, v) {
+			data.push({ id: $(v).data('typeId'), sort: items.length - k });
 		});
-		console.log(data);
 		if (data.length) {
-			ls.ajax.load(aRouter.admin+'settings/topic-type/ajax-sort/', { data: data }, function(result) {
+			ls.ajax.load(aRouter.admin + 'settings/topic-type/ajax-sort/', { data: data }, function(result) {
 				if (result.bStateError) {
 					ls.msg.error(null, result.sMsg);
 				} else {
@@ -44,4 +55,5 @@ ls.admin_topic = ( function ($) {
 	};
 
 	return this;
-}).call(ls.admin_topic || {},jQuery);
+	
+}).call(ls.admin_topic || {}, jQuery);
