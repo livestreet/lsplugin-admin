55c6aab22d380a2bc86725e82c143e62f71239ad
PSNet <light.feel@gmail.com>
2014-08-23 15:40:09 +0300

    - мелкие правки
    Signed-off-by: PSNet <light.feel@gmail.com>

--- include/arrays.php
+++ include/arrays.php
@@ -64,41 +64,43 @@ function array_replace_recursive_distinct() {
 }
 
 
-/**
- * Получить из массива ассоциативных массивов значения по указанному имени столбца
- * tip: аналог array_column из пхп 5.5
- *
- * @param $aArray				массив ассоциативных массивов
- * @param $mColumnKeyName		ключ ассоциативного подмассива для получения значения
- * @param $mIndexKeyName		ключ ассоциативного подмассива для получения значения, которое будет использовано в качестве ключа для получаемого значения
- * @return array				массив значений из указанного столбца
- */
-function my_array_column($aArray, $mColumnKeyName, $mIndexKeyName = null) {
-	$aData = array();
-	foreach($aArray as $aRow) {
-		/*
-		 * есть ли значение
-		 */
-		if (!isset($aRow[$mColumnKeyName])) {
-			continue;
-		}
-		$mValue = $aRow[$mColumnKeyName];
-		/*
-		 * если имя ключа ассоциативного массива указано и это значение существует
-		 */
-		if (!is_null($mIndexKeyName) and isset($aRow[$mIndexKeyName])) {
+if (!function_exists('my_array_column')) {
+	/**
+	 * Получить из массива ассоциативных массивов значения по указанному имени столбца
+	 * tip: аналог array_column из пхп 5.5
+	 *
+	 * @param array $aArray         массив ассоциативных массивов
+	 * @param int   $mColumnKeyName ключ ассоциативного подмассива для получения значения
+	 * @param int   $mIndexKeyName  ключ ассоциативного подмассива для получения значения, которое будет использовано в качестве ключа для устанавливаемого значения в новом массиве
+	 * @return array                массив значений из указанного столбца
+	 */
+	function my_array_column($aArray, $mColumnKeyName, $mIndexKeyName = null) {
+		$aData = array();
+		foreach($aArray as $aRow) {
 			/*
-			 * добавить нужное значение с указанием ключа как значения другого ключа ассоциативного массива
+			 * есть ли значение
 			 */
-			$aData[$aRow[$mIndexKeyName]] = $mValue;
-		} else {
+			if (!isset($aRow[$mColumnKeyName])) {
+				continue;
+			}
+			$mValue = $aRow[$mColumnKeyName];
 			/*
-			 * просто добавить значение
+			 * если имя ключа ассоциативного массива указано и это значение существует
 			 */
-			$aData[] = $mValue;
+			if (!is_null($mIndexKeyName) and isset($aRow[$mIndexKeyName])) {
+				/*
+				 * добавить нужное значение с указанием ключа как значения другого ключа ассоциативного массива
+				 */
+				$aData[$aRow[$mIndexKeyName]] = $mValue;
+			} else {
+				/*
+				 * просто добавить значение
+				 */
+				$aData[] = $mValue;
+			}
 		}
+		return $aData;
 	}
-	return $aData;
 }
 
 ?>
